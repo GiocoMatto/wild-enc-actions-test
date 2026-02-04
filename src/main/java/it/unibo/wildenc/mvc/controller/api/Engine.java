0254a6b14d476b7e0c287122acb6f526da014976
Leonardo Mengozzi <leonardo.mengozzi2@studio.unibo.it>
2026-02-04 22:09:53 +0100

    fix(feature-controller): modified game loop no more polling

--- src/main/java/it/unibo/wildenc/mvc/controller/api/Engine.java
+++ src/main/java/it/unibo/wildenc/mvc/controller/api/Engine.java
@@ -1,6 +1,7 @@
 package it.unibo.wildenc.mvc.controller.api;
 
 import it.unibo.wildenc.mvc.controller.api.InputHandler.MovementInput;
+import it.unibo.wildenc.mvc.model.GameMap;
 
 /**
  * Responsible of handling the main GameLoop and communicate with Views and the Model.
@@ -10,6 +11,12 @@ public interface Engine {
     // esporre solo per aggiungere il metodo.
     void processInput(MovementInput movement);
 
+    void setPause(boolean status);
+
     void onLeveUpChoise(String choise);
+
+    void chosePlayerType(GameMap.PlayerType playerType);
+
+    void startGameLoop();
 
 }
--- src/main/java/it/unibo/wildenc/mvc/controller/impl/EngineImpl.java
+++ src/main/java/it/unibo/wildenc/mvc/controller/impl/EngineImpl.java
@@ -1,94 +1,106 @@
 package it.unibo.wildenc.mvc.controller.impl;
 
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.locks.LockSupport;
 import org.joml.Vector2d;
 import it.unibo.wildenc.mvc.controller.api.Engine;
 import it.unibo.wildenc.mvc.controller.api.MapObjViewData;
 import it.unibo.wildenc.mvc.controller.api.InputHandler.MovementInput;
 import it.unibo.wildenc.mvc.model.GameMap;
+import it.unibo.wildenc.mvc.model.GameMap.PlayerType;
 import it.unibo.wildenc.mvc.model.map.GameMapImpl;
 import it.unibo.wildenc.mvc.view.api.GameView;
 import it.unibo.wildenc.mvc.view.impl.GameViewImpl;
 
 public class EngineImpl implements Engine{
     private final LinkedBlockingQueue<MovementInput> movements = new LinkedBlockingQueue<>();
-    private final LinkedBlockingQueue<String> upgrade = new LinkedBlockingQueue<>();
     private final GameView view = new GameViewImpl();
     private final GameLoop loop = new GameLoop();
-    private final GameMap model;
-    private STATUS status = STATUS.GAME;
-    private boolean running = true;
-
-    public enum STATUS {GAME, PAUSE;}
-
-    public EngineImpl(final GameMap.PlayerType playerType) {
-        model = new GameMapImpl(playerType);
+    private final Object pauseLock = new Object();
+    private volatile STATUS gameStatus = STATUS.RUNNING;
+    private volatile GameMap model;
+    private PlayerType playerType;
+    
+    public enum STATUS {RUNNING, PAUSE;}
+    
+    public EngineImpl() {
         view.setEngine(this);
         view.start();
     }
-
+    
+    @Override
     public void startGameLoop() {
-        this.loop.start();    
+        model = new GameMapImpl(playerType);
+        this.loop.start();
     }
-
+    
     /**
      * {@inheritDoc}
-     */
+    */
     @Override
     public void processInput(final MovementInput movement) {
         this.movements.add(movement);
     }
-
+    
     @Override
     public void onLeveUpChoise(String choise) {
-        this.upgrade.add(choise);
+        //model.aggiornastastisticheplayer.
+        setPause(false);
     }
-
-
+    
+    @Override
+    public void setPause(boolean status) {
+        synchronized (pauseLock) {
+            this.gameStatus = status ? STATUS.PAUSE : STATUS.RUNNING;
+            pauseLock.notifyAll();
+        }
+    }
+    
+    @Override
+    public void chosePlayerType(PlayerType playerType) {
+        this.playerType = playerType;
+    }
+    
     public final class GameLoop extends Thread {
         private static final long SLEEP_TIME = 10;
+        private boolean running = true;
 
         @Override
         public void run() {
-            long lastTime = System.nanoTime();
-            while (running) {
-                final long now = System.nanoTime();
-                final long dt = now - lastTime;
-                lastTime = now;
-                /* condition game state */
-                view.updateSprites(model.getAllObjects().stream()
-                    .map(e -> new MapObjViewData(
-                        "null", 
-                        e.getPosition().x(), 
-                        e.getPosition().y()
-                    ))
-                    .iterator()
-                );
-                LockSupport.parkNanos(SLEEP_TIME);
-                // TODO: fare controllo se aggiungere un arma ogni livello. Nel model 
-                // bisognerebbe aggiungere se è il player ha livellato.
-                /**
-                 * if (model.levelUp() {
-                 *  status = STATUS.PAUSE;
-                 *  view.levelUp();
-                 * }
-                 *//*
-                 * if (model.end()) {
-                 *  view.end();
-                 *  running = false;
-                 * }
-                 */
-                final var levUp = upgrade.poll();
-                if (levUp != null) {
-                    //model.aggiornastastisticheplayer.
-                    status = STATUS.GAME;
-                }
-                if (status == STATUS.GAME) {
+            try {
+                long lastTime = System.nanoTime();
+                while (running) {
+                    synchronized (pauseLock) {
+                        while (gameStatus == STATUS.PAUSE) {
+                            pauseLock.wait();
+                        }
+                    }
+                    final long now = System.nanoTime();
+                    final long dt = now - lastTime;
+                    lastTime = now;
+                    view.updateSprites(model.getAllObjects().stream()
+                        .map(e -> new MapObjViewData(
+                            "null", 
+                            e.getPosition().x(), 
+                            e.getPosition().y()
+                        ))
+                        .iterator()
+                    );
+                    Thread.sleep(SLEEP_TIME);
                     final var move = movements.poll();
                     model.updateEntities(dt, (move != null) ? move.getVector() : new Vector2d(0, 0));
+                    /**
+                     * if (model.levelUp() {
+                     *  setPause(true);
+                     *  view.levelUp();
+                     * }*/
+                    /*
+                    * if (model.end()) {
+                    *  view.end();
+                    *  running = false;
+                    * }
+                    */
                 }
-            }
+            } catch (InterruptedException e1) { }
         }
 
     }
